32e470ab7b311edd9170cdb824c6905a838fb0b2
João Gabriel Santana <joao.santana09@outlook.com>
2025-07-01 23:08:13 -0300

    :package: feat: install and configure tailwind and nativewind :sparkles:

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import "./global.css";
 import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
