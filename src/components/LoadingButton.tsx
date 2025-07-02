8315fb37b94ce57df5b6f806c06f6e0d929cd5b8
João Gabriel Santana <joao.santana09@outlook.com>
2025-07-01 23:08:44 -0300

    :sparkles: feat: create LoadingButton component

---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,32 @@
+import {
+  ActivityIndicator,
+  Text,
+  TextProps,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+
+interface Props extends TouchableOpacityProps {
+  loading?: boolean;
+  text: string;
+  textProps?: TextProps;
+}
+
+export function LoadingButton({
+  loading = false,
+  text,
+  textProps,
+  ...props
+}: Props) {
+  return (
+    <TouchableOpacity {...props}>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text className="color-white font-semibold text-md" {...textProps}>
+          {text}
+        </Text>
+      )}
+    </TouchableOpacity>
+  );
+}
